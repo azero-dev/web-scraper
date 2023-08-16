c9f77b982f76855d6f0a1093188d1fc985c8f01f
Fran Rodriguez <fr.rodmar@gmail.com>
2023-08-17 00:10:46 +0100

    app.js code cleaned

--- app.js
+++ app.js
@@ -5,40 +5,48 @@ const app = express()
 app.use(express.json())
 app.disable('x-powered-by')
 
+// Define the whitelist of allowed origins
+const allowedOrigins = ['http://localhost:8080']
+
 app.use((req, res, next) => {
-  // Change access control to allow requests from your own domain
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  const { origin } = req.headers;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
   next()
 })
 
 app.get('/', async (req, res) => {
-  const { url, option } = req.query;
-  if ( url && option === 'screenshot') {
-    try {
-      const screenshot = await run(url, option)
-      const screenshotBase64 = screenshot.toString('base64');
+  const { url, option } = req.query
+
+  try {
+    if (!url || !option) {
+      return res.status(400).json({ error: 'Missing url or option' })
+    }
+
+    let result;
+    if (option === 'screenshot' || option === 'links') {
+      result = await run(url, option)
+    } else {
+      return res.status(400).json({ error: 'Invalid option' })
+    }
+
+    if (option === 'screenshot') {
+      const screenshotBase64 = result.toString('base64')
       res.writeHead(200, {
         'Content-Type': 'image/png',
       })
       res.end(screenshotBase64, 'base64')
-    } catch (error) {
-      console.error('Error:', error)
-      res.status(500).json({ error: 'An error occurred' })
+    } else if (option === 'links') {
+      return res.status(200).json({ links: result })
     }
-  } else if ( url && option === 'links') {
-    try {
-      const links = await run(url, option)
-      res.json({ links })
-    } catch (error) {
-      console.error('Error:', error)
-      res.status(500).json({ error: 'An error occurred' })
-    }
-  } else {
-    res.send('<h1>Ask me something!</h1>')
+  } catch (error) {
+    console.error('Error:', error)
+    return res.status(500).json({ error: 'An error occurred' })
   }
 })
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
