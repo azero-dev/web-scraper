151ac556e09d4d6f5c10861c402b6fcce89555da
Fran Rodriguez <fr.rodmar@gmail.com>
2023-08-19 21:04:22 +0100

    controller added

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,31 +1,6 @@
 import { Router } from "express"
-import { validateQuery } from "../schemas/request.js"
-import { fetcherModel } from "../models/fetcher.js"
+import { FetcherChontroller } from "../controllers/fetcher.js"
 
 export const indexRouter = Router()
 
-indexRouter.get("/", async (req, res) => {
-  const validation = validateQuery(req.query);
-
-  if (!validation.success) {
-    return res.status(422).json({ error: validation.error.message });
-  }
-
-  try {
-    const { url, option } = validation.data
-    let result = await fetcherModel.run({ url, option })
-
-    if (option === "screenshot") {
-      const screenshotBase64 = result.toString("base64");
-      res.writeHead(200, {
-        "Content-Type": "image/png",
-      });
-      res.end(screenshotBase64, "base64");
-    } else if (option === "links") {
-      return res.status(200).json({ links: result });
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    return res.status(500).json({ error: error.message });
-  }
-})
+indexRouter.get("/", FetcherChontroller.index)
